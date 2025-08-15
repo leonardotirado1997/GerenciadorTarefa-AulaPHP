4a6a44de16ca8a66acd9b33376fa6df566ac2930
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-08-15 09:10:41 -0300

    Primeiro Commit da aula de programação, apenas front-end

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CRUD MYSQL</title>
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">Crud PHP</a>
+            </div>
+        </nav>
+        <main class="container p-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <!-- Formulário -->
+                    <div class="card card-body">
+                        <form action="#" method="POST">
+                            <div class="form-group">
+                                <input type="text" name="titulo" class="form-control"
+                                    placeholder="Titulo da tarefa" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <textarea name="descricao" rows="2" class="form-control"
+                                    placeholder="Descrição">
+                            </textarea>
+                            </div>
+                            <input type="submit" name="salvar" class="btn btn-success btn-block"
+                                value="Salvar Tarefa">
+                        </form>
+                    </div>
+                </div>
+                <!-- /Formulário -->
+
+                <!-- Tabela de Tarefas -->
+                <div class="col-md-8">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Titulo</th>
+                                <th>Descrição</th>
+                                <th>Criado em</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Atividade</td>
+                                <td>Descrição</td>
+                                <td>15-08-2025</td>
+                                <td><a href="#" class="btn btn-secondary">
+                                        <i class="fas fa-marker"></i>
+                                    </a>
+                                    <a href="#" class="btn btn-danger">
+                                        <i class="far fa-trash-alt"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /Tabela de Tarefas -->
+            </div>
+        </main>
+    </div>
+
+    <!-- BOOTSTRAP 4 SCRIPTS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+
+</html>
