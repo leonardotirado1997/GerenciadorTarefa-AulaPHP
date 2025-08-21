f734bed92b0b7f56a387fdeb25d888a27e455363
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-08-21 11:49:51 -0300

    Criando a funcionalidade de deletar tarefa, criando novo arquivo delete.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
                                         <td><a href="#" class="btn btn-secondary">
                                                 <i class="fas fa-marker"></i>
                                             </a>
-                                            <a href="#" class="btn btn-danger">
+                                            <a href="delete.php?id=<?=$row['id']; ?>" class="btn btn-danger">
                                                 <i class="far fa-trash-alt"></i>
                                             </a>
                                         </td>
