52bbc44e9203d25f533f68af38acd0eeddd0eec6
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-09-02 10:55:24 -0300

    Cadastrando usuario e aplicando login, no data base a senha fica criptografada, alem de criar outros arquivos par autenticar as paginas em php

--- conn.php
+++ conn.php
@@ -9,5 +9,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexão falhou " . $conn->connect_error);
-    
+}
+
+$sql = "CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )";
+
+if ($conn->query($sql) === FALSE) {
+    echo "Erro ao criar tabela: " . $conn->error;
 }
